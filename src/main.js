b7201d1a0765c6d66a8f9ef0080fce802284d5ba
Viktor Blidh <55929893+vblidh@users.noreply.github.com>
2020-10-11 19:34:48 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import VueCookies from 'vue-cookies'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
+axios.default.baseURL = "https://ergast.com/api"
+Vue.use(VueCookies)
+Vue.use(VueAxios, axios)
+
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
