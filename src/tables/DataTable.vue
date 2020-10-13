1d7207c697fe799d95449f2a22f04789c49354c2
Viktor Blidh <55929893+vblidh@users.noreply.github.com>
2020-10-14 00:10:18 +0200

    Create first table with latest race reults

---
+++ src/tables/DataTable.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <h1 class="title">{{this.Title}}</h1>
+    <v-data-table
+      :headers="tableHeaders"
+      :items="tableData"
+      item-key="Name"
+      class="elevation-1"
+      items-per-page="20"
+      hide-default-footer
+      @click:row="onRowClicked"
+    >
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    headers: Array,
+    data: Array,
+  },
+  data() {
+    return {
+      Title: this.title,
+      tableData: this.data,
+      tableHeaders: this.headers,
+    };
+  },
+  methods: {
+    onRowClicked(par1, par2) {
+      console.log(par1);
+      console.log(par2);
+    },
+  },
+  mounted() {
+    console.log("Title:", this.Title);
+    console.log("Tabledata: ", this.tableData);
+    console.log("Table headers: ", this.tableHeaders);
+  },
+  beforeMount() {
+    console.log("Title:", this.Title);
+    console.log("Tabledata: ", this.tableData);
+    console.log("Table headers: ", this.tableHeaders);
+  },
+};
+</script>
+
+<style scoped>
+
+.title {
+    text-align: center;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,73 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DataTable v-if="gotData" :title="this.Title" :headers="this.tableHeaders" :data="this.tableData"></DataTable>
+    <v-btn @click="getData">Collect Data</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import DataTable from "@/tables/DataTable.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTable,
+  },
+  data() {
+    return {
+      gotData: false,
+      Title:  "",
+      data: {},
+      tableData: [],
+      tableHeaders: [
+        { text : "Position", value : "Position"}, 
+        { text : "Name", value: "Name"}, 
+        { text : "Team", value:"Team"}, 
+        { text : "Time", value : "Time"}, 
+        { text : "Points", value : "Points"} 
+        ],
+    };
+  },
+  methods: {
+    getData() {
+      this.data = {};
+      this.tableData = [];
+      this.axios
+        .get("/f1/current/last/results.json")
+        .then((resp) => resp.data)
+        .then((data) => (this.data = data.MRData))
+        .finally(() => {
+          console.log("Data: ", this.data);
+          var results = this.data.RaceTable.Races[0].Results;
+          for (var i = 0; i < results.length; i++) {
+            //console.log(results[i]);
+            var driver = results[i].Driver;
+            var name = driver.givenName + " " + driver.familyName;
+            var pos = results[i].position;
+            var constructor = results[i].Constructor.name;
+            var time;
+            if (results[i].status == "Finished") {
+              time = results[i].Time.time;
+            } else {
+              time = "DNF";
+            }
+            var points = results[i].points;
+            var obj = {
+              Position: pos,
+              Name: name,
+              Team: constructor,
+              Time: time,
+              Points: points,
+            };
+            console.log(obj);
+            this.tableData.push(obj);
+          }
+          console.log(this.tableData);
+          this.gotData = true;
+          this.Title = this.data.RaceTable.Races[0].raceName;
+          console.log(this.gotData);
+        });
+    },
+  },
+};
 </script>
