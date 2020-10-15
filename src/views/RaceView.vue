d8d53e0886c05db4cb5080343ff9f33432ae255c
Viktor Blidh <55929893+vblidh@users.noreply.github.com>
2020-10-15 20:44:18 +0200

    Add caching of Race results

--- src/views/RaceView.vue
+++ src/views/RaceView.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <v-row>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
       <v-col>
         <v-select
           hint="Choose a year"
@@ -11,15 +11,26 @@
           @change="getRacesByYear"
         ></v-select>
       </v-col>
-      <v-col cols="8">
+      <v-col cols="6">
         <v-select
           hint="Choose a circuit"
           label="Round"
           :items="Races"
           v-model="Round"
           outlined
-          @change="getRaceResults"
         ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          label="Session"
+          :items="Sessions"
+          v-model="ChosenSession"
+          outlined
+        >
+        </v-select>
+      </v-col>
+      <v-col>
+        <v-btn x-large @click="getResults">Collect Data</v-btn>          
       </v-col>
     </v-row>
     <v-row>
@@ -68,6 +79,8 @@ export default {
       Round: this.round,
       Races: [],
       Seasons: [],
+      ChosenSession: "",
+      Sessions: ["Qualifying", "Race"],
     };
   },
   beforeMount() {
@@ -89,6 +102,21 @@ export default {
     },
   },
   methods: {
+    addResultToCache(newKey) {
+      var cache = localStorage.getItem("RaceResults");
+      var parsed;
+      if (cache != null) {
+        console.log("Updating race result cache with key", newKey);
+        parsed = JSON.parse(cache);
+        parsed[newKey] = this.data;
+        localStorage.setItem("RaceResults", JSON.stringify(parsed));
+      } else {
+        console.log("Creating new cache of race results with key", newKey);
+        parsed = {};
+        parsed[newKey] = this.data;
+        localStorage.setItem("RaceResults", JSON.stringify(parsed));
+      }
+    },
     getRacesByYear() {
       var cachedRaces = localStorage.getItem("Races");
       var isDataCached = false;
@@ -135,83 +163,82 @@ export default {
           });
       }
     },
-    getRaceResults() {
+    getResults() {
+      if (this.ChosenSession === "Race") {
+        this.getRaceResults();
+      }
+      else {
+        console.log("Getting qualification results");
+      }
+    },
+    async getRaceResults() {
       this.tableData = [];
       var cacheExists = false;
       var cachedRaceResults = localStorage.getItem("RaceResults");
+      var raceKey = this.Year + "_" + this.Round;
 
       if (cachedRaceResults != null) {
         var parsed = JSON.parse(cachedRaceResults);
-        var desiredKey = this.Year + "_" + this.Round;
-        if (parsed[desiredKey] != null) {
-          this.tableData = Object.assign(this.tableData, parsed[desiredKey]);
+        if (parsed[raceKey] != null) {
+          this.data = parsed[raceKey];
+          console.log("Found cached raceresults with key", raceKey);
           cacheExists = true;
         }
       }
       var url = this.Year + "/" + this.Round + "/results.json";
+      if (!cacheExists) {
+        await this.axios
+          .get(url)
+          .then((resp) => resp.data)
+          .then((data) => (this.data = data.MRData));
+        this.addResultToCache(raceKey);
+      }
 
-      this.axios
-        .get(url)
-        .then((resp) => resp.data)
-        .then((data) => (this.data = data.MRData))
-        .then(() => {
-          var results = this.data.RaceTable.Races[0].Results;
-          var tmp = [];
-          for (var i = 0; i < results.length; i++) {
-            var driver = results[i].Driver;
-            var name = driver.givenName + " " + driver.familyName;
-            var pos = results[i].position;
-            var constructor = results[i].Constructor.name;
-            var time;
-            var status = results[i].status;
-            if (status === "Finished") {
-              time = results[i].Time.time;
-            } else if (status.startsWith("+")) {
-              time = status;
-            } else {
-              time = "DNF (" + status + ")";
-            }
-            var points = results[i].points;
-            var obj = {
-              Position: pos,
-              Name: name,
-              Team: constructor,
-              Time: time,
-              Points: points,
-            };
-            tmp.push(obj);
-          }
-          this.tableData = tmp;
-          console.log("Tabledata after: ", this.tableData);
-          var newKey = this.Year + "_" + this.Round;
-          if (cachedRaceResults != null) {
-            parsed[newKey] = this.tableData;
-            console.log("Updating race result cache with key", newKey);
-            localStorage.setItem("RaceResults", JSON.stringify(parsed));
-          } else {
-            parsed = {}
-            parsed[newKey] = this.tableData;
-            console.log("Creating new cache of race results with key", newKey);
-            localStorage.setItem("RaceResults", JSON.stringify(parsed));
-          }
-          this.tableHeaders = [
-            {
-              text: "Position",
-              value: "Position",
-              divider: true,
-              align: "center",
-              width: "4",
-            },
-            { text: "Name", value: "Name", divider: true, align: "center" },
-            { text: "Team", value: "Team", divider: true, align: "center" },
-            { text: "Time", value: "Time", divider: true, align: "center" },
-            { text: "Points", value: "Points", divider: true, align: "center" },
-          ];
-          this.RaceName = this.data.RaceTable.Races[0].raceName;
-          this.Circuit = this.data.RaceTable.Races[0].Circuit.circuitName;
-          this.RaceDate = this.data.RaceTable.Races[0].date;
-          this.showTable = true;
-        });
+      var results = this.data.RaceTable.Races[0].Results;
+      var tmp = [];
+      for (var i = 0; i < results.length; i++) {
+        var driver = results[i].Driver;
+        var name = driver.givenName + " " + driver.familyName;
+        var pos = results[i].position;
+        var constructor = results[i].Constructor.name;
+        var time;
+        var status = results[i].status;
+        if (status === "Finished") {
+          time = results[i].Time.time;
+        } else if (status.startsWith("+")) {
+          time = status;
+        } else {
+          time = "DNF (" + status + ")";
+        }
+        var points = results[i].points;
+        var obj = {
+          Position: pos,
+          Name: name,
+          Team: constructor,
+          Time: time,
+          Points: points,
+        };
+        tmp.push(obj);
+      }
+      this.tableData = tmp;
+
+      this.tableHeaders = [
+        {
+          text: "Position",
+          value: "Position",
+          divider: true,
+          align: "center",
+          width: "4",
+        },
+        { text: "Name", value: "Name", divider: true, align: "center" },
+        { text: "Team", value: "Team", divider: true, align: "center" },
+        { text: "Time", value: "Time", divider: true, align: "center" },
+        { text: "Points", value: "Points", divider: true, align: "center" },
+      ];
+      this.RaceName = this.data.RaceTable.Races[0].raceName;
+      this.Circuit = this.data.RaceTable.Races[0].Circuit.circuitName;
+      this.RaceDate = this.data.RaceTable.Races[0].date;
+      this.showTable = true;
     },
   },
 };
